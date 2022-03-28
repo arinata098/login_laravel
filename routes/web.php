6e88674658af7971833f596215b44a45846747e4
Bagus Arinata <arinata098@gmail.com>
2022-03-28 10:37:21 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,73 @@
+<?php
+
+use App\Models\Category;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\DashboardController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home', [
+        "title" => "Home",
+        "active" => 'home'
+    ]);
+});
+
+
+Route::get('/about', [PostController::class, 'aboutme']);
+
+Route::get('/posts', [PostController::class, 'index']);
+
+// halaman single post
+Route::get('/posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('/categories', function() {
+    return view('categories', [
+        'title' => 'Post Categories',
+        'active' => 'categories',
+        'categories' => Category::all(),
+    ]);
+});
+
+Route::get('/login', [LoginController::class, 'index'])->middleware('guest')->name('login');
+Route::post('/login', [LoginController::class, 'authenticate']);
+Route::post('/logout', [LoginController::class, 'logout']);
+
+Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
+Route::post('/register', [RegisterController::class, 'store']);
+
+// Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
+Route::get('/dashboard', function() {
+    return view('dashboard.index');
+})->middleware('auth');
+
+// sudah tidak kepake karena sudah di tangani di model
+// Route::get('/categories/{category:slug}', function(Category $category) {
+//     return view('posts', [
+//         'title' => "Post by Category : $category->name",
+//         'active' => 'categories',
+//         'posts' => $category->posts->load('category', 'author')
+//     ]);
+// });
+
+// sudah tidak kepake karena sudah di tangani di model
+// Route::get('/authors/{author:username}', function(User $author) {
+//     return view('posts', [
+//         'title' => "Post by Author : $author->name",
+//         'active' => 'posts',
+//         //eloquent lazy eager loading menggunakan load agar queri 1x saja tidak berulang''
+//         'posts' => $author->posts->load('category', 'author'),
+//     ]);
+// });
